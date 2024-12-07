87891711f467a7e785e3d1722112f00df1774016
Muntasir Udoy <muntasirudoy4@gmail.com>
2024-12-07 15:20:07 +0600

    update

--- .exlintrc.js
+++ .exlintrc.js
@@ -3,6 +3,8 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     " @typescript-eslint/no-unsafe-function-type": "off",
     "react-hooks/exhaustive-deps": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "prefer-const": "off",
     // Disables the rule globally
     // Turns the error into a warning
   },
